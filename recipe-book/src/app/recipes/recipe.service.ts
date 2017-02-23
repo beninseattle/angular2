f0cc6806fefeca3a820896088561bd9ef58f63b2
Benjamin Adair <badair@MacBook-Pro.local>
2017-02-23 12:45:38 -0800

    Lecture 70: Cross service communication

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient';
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Turkey', 'Roast turkey', 'https://thumbs.dreamstime.com/z/wild-turkey-1776418.jpg', []),
-    new Recipe('Turkey2', 'Roast turkey2', 'https://s3.amazonaws.com/graphics.texastribune.org/graphics/turkey-facts/assets/turkey-beard.jpg', [])
+    new Recipe('Turkey', 'Roast turkey', 'https://thumbs.dreamstime.com/z/wild-turkey-1776418.jpg', [
+      new Ingredient('Turkey', 1),
+      new Ingredient('Breadcrumbs', 5),
+      new Ingredient('Spices', 10)
+    ]),
+    new Recipe('Turkey2', 'Roast turkey2', 'https://s3.amazonaws.com/graphics.texastribune.org/graphics/turkey-facts/assets/turkey-beard.jpg', [
+      new Ingredient('Turkey', 1),
+      new Ingredient('Breadcrumbs', 5),
+      new Ingredient('Gravy', 3)
+    ])
   ];
 
   constructor() { }
