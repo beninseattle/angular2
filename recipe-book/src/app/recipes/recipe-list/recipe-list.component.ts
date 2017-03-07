283a81360f9e6c33fb8b7cf9bfd259555ef5c2f7
Benjamin Adair <bena@mac.com>
2017-03-07 11:18:20 -0800

    Lecture 96 Child routes in recipes section

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,16 +8,10 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
-  @Output() selectRecipe = new EventEmitter<Recipe>();
-  //recipe: Recipe = new Recipe('Turkey', 'Roast turkey', 'https://thumbs.dreamstime.com/z/wild-turkey-1776418.jpg');
 
   constructor (private recipeService: RecipeService) { }
 
   ngOnInit () {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onSelected (recipe: Recipe) {
-    this.selectRecipe.emit(recipe);
   }
 }
